ef0d2c1ad43f85131f58c442d4c1491deebb4f9c
Mldorlfuse <sania.4game@gmail.com>
2025-10-31 11:38:05 +0300

    1

--- conftest.py
+++ conftest.py
@@ -1,7 +1,26 @@
-from selene import have, be
-from selene.support.shared import browser
 import pytest
+from selenium import webdriver
+from selene.support.shared import browser
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_browser():
+    # Настройки Chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Без графического интерфейса
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Установка драйвера через WebDriver Manager
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+    # Настройка Selene
+    browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.timeout = 10
+
+    yield browser
+
+    driver.quit()
